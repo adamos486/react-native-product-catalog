edc2564510de4b9063e04713d000f3c35c36aa17
Adam Cobb <adamos486@gmail.com>
2025-03-02 13:23:33 -0800

    Commiting first changes to App.tsx to see them run in the iOS simulator.

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Image
+        source={{
+          uri: 'https://picsum.photos/200',
+        }}
+        style={{
+          width: 300,
+          height: 400,
+          resizeMode: 'cover',
+          justifyContent: 'center'
+        }} />
       <StatusBar style="auto" />
     </View>
   );
