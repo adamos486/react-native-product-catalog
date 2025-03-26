ef655d1884e5a7c5011c67c73f16c9fd89e3020d
TJ Maynes <tj@tjmaynes.com>
2025-03-26 10:23:12 -0500

    chore: add useAppLoader hook, remove 'expo-app-loading is deprecated in favour of expo-splash-screen' warning

--- App.tsx
+++ App.tsx
@@ -1,33 +1,18 @@
 import {Home, Catalog} from './screens'
 import {NavigationContainer} from '@react-navigation/native'
 import {createStackNavigator} from '@react-navigation/stack'
-import * as Font from 'expo-font'
-import AppLoading from 'expo-app-loading'
-import {useState} from 'react'
 import type { RootStackParamList } from './types'
+import {useAppLoader} from "./hooks/useAppLoader";
 
 const Stack = createStackNavigator<RootStackParamList>()
 
-const fetchFonts = () => {
-    return Font.loadAsync({
-        NonSeasonal: require('./assets/fonts/Non-Seasonal_Geo-Medium_web.ttf'),
-    })
-}
-
 const App = () => {
-    const [fontLoaded, setFontLoaded] = useState(false)
+    const { isAppReady, onReadyCallback } = useAppLoader()
 
-    if (!fontLoaded) {
-        return (
-            <AppLoading
-                startAsync={fetchFonts}
-                onFinish={() => setFontLoaded(true)}
-                onError={console.warn}
-            />
-        )
-    }
+    if (!isAppReady) return null;
+
     return (
-        <NavigationContainer>
+        <NavigationContainer onReady={onReadyCallback}>
             <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
                 <Stack.Screen name="Home" component={Home}/>
                 <Stack.Screen name="Catalog" component={Catalog}/>
@@ -35,4 +20,5 @@ const App = () => {
         </NavigationContainer>
     )
 }
+
 export default App
---
+++ hooks/useAppLoader.tsx
@@ -0,0 +1,44 @@
+import * as SplashScreen from 'expo-splash-screen';
+import {useCallback, useEffect, useState} from "react";
+import * as Font from "expo-font";
+
+const fetchFonts = () => {
+    return Font.loadAsync({
+        NonSeasonal: require('../assets/fonts/Non-Seasonal_Geo-Medium_web.ttf'),
+    })
+}
+
+export const useAppLoader = () => {
+    const [appIsReady, setAppIsReady] = useState(false);
+
+    useEffect(() => {
+        const prepare = async () => {
+            try {
+                // Keep the splash screen visible while we fetch resources
+                await SplashScreen.preventAutoHideAsync();
+                // Pre-load fonts, make any API calls you need to do here
+                await fetchFonts()
+            } catch (e) {
+                console.warn(e);
+            } finally {
+                // Tell the application to render
+                setAppIsReady(true);
+            }
+        }
+
+        prepare()
+    }, []);
+
+    const onLayoutRootView = useCallback(async () => {
+        if (appIsReady) {
+            // This tells the splash screen to hide immediately! If we call this after
+            // `setAppIsReady`, then we may see a blank screen while the app is
+            // loading its initial state and rendering its first pixels. So instead,
+            // we hide the splash screen once we know the root view has already
+            // performed layout.
+            await SplashScreen.hideAsync();
+        }
+    }, [appIsReady]);
+
+    return { isAppReady: appIsReady, onReadyCallback: onLayoutRootView }
+}
